94a603067f59542c28935c26f19614452acd3843
Cypo <voodoo.7@outlook.com>
2020-04-20 16:56:16 +0800

    add swagger

--- src/main/java/cn/edu/bjtu/ebosgatewayconfig/dao/DeviceprofileRepository.java
+++ src/main/java/cn/edu/bjtu/ebosgatewayconfig/dao/DeviceprofileRepository.java
@@ -1,8 +1,6 @@
 package cn.edu.bjtu.ebosgatewayconfig.dao;
 
-import cn.edu.bjtu.ebosgatewayconfig.entity.Command;
 import cn.edu.bjtu.ebosgatewayconfig.entity.Deviceprofile;
-import jdk.nashorn.internal.runtime.doubleconv.DoubleConversion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
