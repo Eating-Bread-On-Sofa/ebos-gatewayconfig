d937c4645821c74d68b36e47b8f1d0cec492a3a5
Li-111 <358111907@qq.com>
2020-10-28 21:55:24 +0800

    Log Infornation update

--- src/main/java/cn/edu/bjtu/ebosgatewayconfig/controller/GatewayConfigController.java
+++ src/main/java/cn/edu/bjtu/ebosgatewayconfig/controller/GatewayConfigController.java
@@ -436,7 +436,6 @@ public class GatewayConfigController {
     @CrossOrigin
     @GetMapping("/ping")
     public String ping(){
-        logService.info("retrieve","对网关管理进行了一次健康检测");
         return "pong";
     }
 }
